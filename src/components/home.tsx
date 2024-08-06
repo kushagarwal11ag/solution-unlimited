c2be8db596af1e9d892e33105d07fc4350df9746
Khanjan Dalwadi <17khanjan@gmail.com>
2024-08-07 00:17:33 +0530

    Edited content

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,43 +2,47 @@ import Image from "next/image";
 import Link from "next/link";
 
 const home = () => {
-	return (
-		<section
-			id="home"
-			className="py-10 lg:py-0 min-h-screen flex items-center dark:bg-black"
-		>
-			<div className="container p-4 md:px-6">
-				<div className="grid gap-6 grid-cols-1 lg:grid-cols-2 lg:gap-12">
-					<div className="flex flex-col gap-5">
-						<h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-							Trusted Solutions for Your Business
-						</h1>
-						<p className="md:text-xl">
-							Solution Unlimited provides comprehensive
+  return (
+    <section
+      id="home"
+      className="py-10 lg:py-0 min-h-screen flex items-center dark:bg-black"
+    >
+      <div className="container p-4 md:px-6">
+        <div className="grid gap-6 grid-cols-1 lg:grid-cols-2 lg:gap-12">
+          <div className="flex flex-col gap-5">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+              Trusted Solutions for Your Business
+            </h1>
+            <p className="md:text-xl">
+              {/* Solution Unlimited provides comprehensive
 							electrical, civil, and many more services to ensure
 							efficiency, safety and minimal disruption which
-							helping your business thrive.
-						</p>
-						<Link
-							href="#services"
-							className="w-fit inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 h-10 px-4 py-2 bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90"
-						>
-							Learn More
-						</Link>
-					</div>
-					<div className="relative overflow-hidden h-80">
-						<Image
-							src="/hero.jpg"
-							layout="fill"
-							objectFit="cover"
-							alt="Hero"
-							className="rounded-xl"
-						/>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+							helping your business thrive. */}
+              Welcome to Solution Unlimited, your premier choice for
+              comprehensive commercial and industrial services. Founded by Manoj
+              Kumar and Mukesh Vaghela, we specialize in electrical, civil, and
+              fabrication work, offering seamless, end-to-end solutions.
+            </p>
+            <Link
+              href="#services"
+              className="w-fit inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 h-10 px-4 py-2 bg-slate-900 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90"
+            >
+              Learn More
+            </Link>
+          </div>
+          <div className="relative overflow-hidden h-80">
+            <Image
+              src="/hero.jpg"
+              layout="fill"
+              objectFit="cover"
+              alt="Hero"
+              className="rounded-xl"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default home;
--- src/components/work.tsx
+++ src/components/work.tsx
@@ -2,62 +2,67 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 
 const work = () => {
-	return (
-		<section
-			id="portfolio"
-			className="py-16 md:py-32 dark:bg-black container px-4 md:px-6"
-		>
-			<div className="container px-4 md:px-6">
-				<div className="flex flex-col items-center justify-center text-center">
-					<div className="inline-block rounded-lg px-3 py-1 text-sm text-black/75 dark:text-white/75">
-						Our Work
-					</div>
-					<h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-						Showcasing Our Expertise
-					</h2>
-					<p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-						Take a look at some of our past projects and see how
-						we&apos;ve helped businesses like yours succeed.
-					</p>
-				</div>
-				<div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-3 lg:gap-8">
-					<div className="relative overflow-hidden rounded-xl h-40">
-						<Image
-							src="/fabrication.jpg"
-							layout="fill"
-							objectFit="cover"
-							alt="Project 1"
-						/>
-						<div className="absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 transition-opacity hover:opacity-100">
-							<Button variant="default">View Project</Button>
-						</div>
-					</div>
-					<div className="relative overflow-hidden rounded-xl h-40">
-						<Image
-							src="/plumbing.jpg"
-							layout="fill"
-							objectFit="cover"
-							alt="Project 2"
-						/>
-						<div className="absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 transition-opacity hover:opacity-100">
-							<Button variant="default">View Project</Button>
-						</div>
-					</div>
-					<div className="relative overflow-hidden rounded-xl h-40">
-						<Image
-							src="/electric.jpg"
-							layout="fill"
-							objectFit="cover"
-							alt="Project 3"
-						/>
-						<div className="absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 transition-opacity hover:opacity-100">
-							<Button variant="default">View Project</Button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section
+      id="portfolio"
+      className="py-16 md:py-32 dark:bg-black container px-4 md:px-6"
+    >
+      <div className="flex flex-col items-center justify-center text-center">
+        <div className="container px-4 md:px-6">
+          <div className="flex flex-col items-center justify-center text-center">
+            <div className="inline-block rounded-lg px-3 py-1 text-sm text-black/75 dark:text-white/75">
+              Our Work
+            </div>
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+              Showcasing Our Expertise
+            </h2>
+            <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+              Join our many satisfied clients and experience the convenience and
+              reliability of a partner dedicated to your success.
+            </p>
+          </div>
+          <div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-3 lg:gap-8">
+            <div className="relative overflow-hidden rounded-xl h-40">
+              <Image
+                src="/fabrication.jpg"
+                layout="fill"
+                objectFit="cover"
+                alt="Project 1"
+              />
+              <div className="absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 transition-opacity hover:opacity-100">
+                <Button variant="default">View Project</Button>
+              </div>
+            </div>
+            <div className="relative overflow-hidden rounded-xl h-40">
+              <Image
+                src="/plumbing.jpg"
+                layout="fill"
+                objectFit="cover"
+                alt="Project 2"
+              />
+              <div className="absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 transition-opacity hover:opacity-100">
+                <Button variant="default">View Project</Button>
+              </div>
+            </div>
+            <div className="relative overflow-hidden rounded-xl h-40">
+              <Image
+                src="/electric.jpg"
+                layout="fill"
+                objectFit="cover"
+                alt="Project 3"
+              />
+              <div className="absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 transition-opacity hover:opacity-100">
+                <Button variant="default">View Project</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p className="text-3xl font-bold tracking-tighter">
+          Solution Unlimited - Empowering Industries, Anytime, Anywhere.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default work;
